7372a53bce614c8a988b149040669f775d6b05ca
Edson <edbdelima@gmail.com>
2023-10-04 22:32:43 -0300

    Including Q9

---
+++ SolveProblems/Questions/Q9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Q9
+{
+
+    public static string Similar(string word)
+    {
+        int zCount = 0, oCount = 0;
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] == 'z') zCount++;
+            else if (word[i] == 'o') oCount++;
+        }
+
+        return zCount * 2 == oCount ? "Yes" : "No";
+    }
+
+    public static void Main()
+    {
+        string word = Console.ReadLine()!;
+
+        Console.WriteLine(Similar(word));
+    }
+}
